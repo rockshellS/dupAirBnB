1b97929348ee6da25ad28e18c9b75fd2d33c96e8
rockshellS <spicer.rachelle@gmail.com>
2022-01-29 17:19:42 -0500

    refactored code, finished css

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Hero from "./components/Hero"
 import Card from "./components/Card"
 import Data from "./data"
 
+
+
 const data = Data.map( card => {
-   console.log(card.coverImg)
-  return  <Card   key={card.id} id={card.id} title={card.title} description={card.description} 
-price={card.price} coverImg={card.coverImg} stats={card.stats} 
-location={card.location} />
-})
+   return  <Card   
+                key={card.id} 
+                id={card.id} 
+                {...card}
+                
+            />
+    })
+
+
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
-            {/* <Card/> */}
-            {data}
-            
+            <section className="cards-list">
+                {data}
+            </section>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,36 @@
 import React from "react"
 
 
-
 function Card(props) {
-    console.log(props)
+   
+    
+
+    let badgeText 
+    if (props.openSpots === 0) {
+        badgeText = "SOLDOUT"
+    }else if(props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
+
+
+
+
+
     return (
-        <div className="cards">
-            <h3 className="soldout--text">Soldout</h3> 
-            <img src={`../images/${props.coverImg}`} alt="photo" className="card--photo"/>
-            
+
+        <div className="card">
+            { badgeText && <div className="card--badge">{badgeText}</div> } 
+            <img src={`../images/${props.coverImg}`} className="card--image"/>
+
             <div className="card--stats">
-                <img src={process.env.PUBLIC_URL  + "/images/star.png"} alt="Red Star" className="star--photo"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) •</span>
+                <img src={process.env.PUBLIC_URL  + "/images/star.png"} alt="Red Star" className="card--star"/>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) •</span>
                 <span className="gray">{props.location}</span>
             </div>
-            <p className="title--text">{props.title}</p>
-            <p className="text--price"><span className="bold">From ${props.price}</span> / person</p> 
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p> 
         </div>
     )
 
@@ -24,3 +38,5 @@ function Card(props) {
 
 export default Card;
 
+// line 9- wrapping in brackets allows us to do JS so if the openSpots ever equals 0 it will change to sold out. 
+// if i did write this conditional all images would say soldout
